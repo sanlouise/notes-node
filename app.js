6e96f0996fdf504b5aedd7b6ca7495d1286cb94a
sanlouise <sandra.adamshallie@gmail.com>
2017-07-24 19:46:24 -0400

    Require os and fs

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes')
+
+let user = os.userInfo();
+let res = notes.add(2, 3);
+
+console.log('Starting app');
+
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+console.log(res);
