707ce2607095ac2a7c507a909c992aa995fef006
sanlouise <sandra.adamshallie@gmail.com>
2017-07-25 09:46:22 -0400

    Add commands

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes');
-const filteredArray = _.uniq([2, 2, 2, 23, 1]);
+const fs = require('fs');
+let command = process.argv[2]; //list
 
+console.log(process.argv);
 
-let user = os.userInfo();
-let res = notes.add(2, 3);
-
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
-
-
-console.log(_.isString(true));
-console.log(_.isString('true'));
-console.log(filteredArray);
-console.log('Starting app');
-console.log(res);
+if (command === 'add') {
+  console.log('adding a note')
+} if (command === 'list') {
+  console.log('listing all notes');
+} else {
+  console.log('command not found');
+}
