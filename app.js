1efb8ce7287db3e9c2b64f3ec379e8f9c23dccf0
sanlouise <sandra.adamshallie@gmail.com>
2017-07-25 09:30:11 -0400

    Use lodash and install nodemon

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes')
+const notes = require('./notes');
+const filteredArray = _.uniq([2, 2, 2, 23, 1]);
+
 
 let user = os.userInfo();
 let res = notes.add(2, 3);
 
-console.log('Starting app');
-
 fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
+
+
+console.log(_.isString(true));
+console.log(_.isString('true'));
+console.log(filteredArray);
+console.log('Starting app');
 console.log(res);
