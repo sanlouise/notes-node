7bdb520c4860f6591debe3d9591a79ad0ffbff26
sanlouise <sandra.adamshallie@gmail.com>
2017-07-26 10:21:27 -0400

    Store notes

--- notes.js
+++ notes.js
@@ -2,12 +2,24 @@ console.log('Starting notes.js');
 const fs = require('fs');
 
 const addNote = (title, body) => {
+
   let notes = [];
   let note = {
     title,
     body,
   };
 
+  //Check if there are notes with invalid properties
+  try {
+    //Fetch existing notes
+    const existingNotesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(existingNotesString);
+  } catch(e) {
+
+  }
+
+
+  //Push new note
   notes.push(note);
   fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 
